f1dd1a51d44d143859eb4a330a255b237b5b4ca0
DianeShin <jadore845@snu.ac.kr>
2023-08-01 22:43:16 +0200

    add login route

--- jump-booster-frontend/src/App.js
+++ jump-booster-frontend/src/App.js
@@ -1,13 +1,15 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Routes, Route, Link, BrowserRouter} from 'react-router-dom';
-import './App.css';
+import React from 'react';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
 
-import SelectAccount from "./SelectAccount";
+import SelectAccount from "./Auth/SelectAccount";
+import AccountLogin from "./Auth/AccountLogin";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<SelectAccount/>}/>
+          <Route path="/individual-login" element={<AccountLogin role="individual"/>}/>
+          <Route path="/business-login" element={<AccountLogin role="business"/>}/>
       </Routes>
     </BrowserRouter>
   );
