1b57e4d3da8a2edabc501f916c01782b87e95d8c
DianeShin <jadore845@snu.ac.kr>
2023-08-02 19:55:04 +0200

    implement getAccountById

---
+++ jump-booster-frontend/src/Helper/accountHelper.js
@@ -0,0 +1,24 @@
+// fetch user from userId. null if not found.
+export function getAccountById(userId) {
+    const data = {
+        userId: userId
+    };
+
+    return fetch("/account/get-account-by-id", { // Step 1: Return the fetch promise
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userId)
+    })
+        .then((response) => {
+            if (response.status === 200) return response.json();
+            else throw new Error("User not found");
+        })
+        .then((accountObj) => {
+            return accountObj; // Step 2: Return the userObj
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+}
