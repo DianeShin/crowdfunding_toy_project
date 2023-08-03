20892b338f449931b7d2168e409d4ed24a65efb3
DianeShin <jadore845@snu.ac.kr>
2023-08-03 23:35:25 +0200

    add context provider

--- jump-booster-frontend/src/index.js
+++ jump-booster-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import AuthProvider from "./Auth/Authenticator";
+import App from './general/App';
+import AuthProvider from "./general/ContextElem";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
