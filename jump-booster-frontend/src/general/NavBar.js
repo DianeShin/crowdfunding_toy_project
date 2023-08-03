a28c3562763761cf7dc054ff97167bf1c1292255
DianeShin <jadore845@snu.ac.kr>
2023-08-03 23:31:44 +0200

    implement navigation bar

---
+++ jump-booster-frontend/src/general/NavBar.js
@@ -0,0 +1,29 @@
+import {Link, Outlet} from "react-router-dom";
+import {useContext} from "react";
+import {ContextProvider} from "./ContextElem";
+import './NavBar.css'
+export const NavBar = () => {
+    const {userId} = useContext(ContextProvider);
+
+    return (
+        <>
+            <nav className = "navigationBar">
+                <Link className = "navBarLink" to = "/">Home</Link>
+                <Link className = "navBarLink" to = "/project-posts">projects</Link>
+
+                {userId === '' ? (
+                    <>
+                        <Link id = "signInNavBarLink" className = "navBarLink" to = "/select-account">Sign In</Link>
+                    </>
+                ) : (
+                    <>
+                        <Link id = "signInNavBarLink" className = "navBarLink" to = "/sign-out">Sign Out</Link>
+                    </>
+                )}
+            </nav>
+            <Outlet/>
+        </>
+    )
+}
+
+export default NavBar;
