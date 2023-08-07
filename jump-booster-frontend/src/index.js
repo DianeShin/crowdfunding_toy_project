a2d54f9a2465ff02947b49bf0c269163b7449500
DianeShin <jadore845@snu.ac.kr>
2023-08-07 22:01:32 +0200

    move BrowserRouter location

--- jump-booster-frontend/src/index.js
+++ jump-booster-frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './general/App';
 import AuthProvider from "./general/ContextElem";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AuthProvider>
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Battambang:wght@700&family=Rokkitt:wght@300&display=swap');
-        </style>
-        <App />
-    </AuthProvider>
+    <BrowserRouter>
+        <AuthProvider>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Battambang:wght@700&family=Rokkitt:wght@300&display=swap');
+            </style>
+            <App />
+        </AuthProvider>
+    </BrowserRouter>
 );
