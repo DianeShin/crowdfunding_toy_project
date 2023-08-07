25366348e1b1f26a8b90400cfa92b4297253b43d
DianeShin <jadore845@snu.ac.kr>
2023-08-07 21:56:28 +0200

    implement backend for business user post view

--- jump-booster-backend/src/main/java/com/jumpbooster/jumpboosterbackend/post/PostRepository.java
+++ jump-booster-backend/src/main/java/com/jumpbooster/jumpboosterbackend/post/PostRepository.java
@@ -1,14 +1,13 @@
 package com.jumpbooster.jumpboosterbackend.post;
 
-import com.jumpbooster.jumpboosterbackend.individual.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query("SELECT p FROM Post p WHERE p.postId = ?1")
-    public Optional<Post> selectPostFromId(Long postId);
+    @Query("SELECT p FROM Post p WHERE p.userId = ?1")
+    public List<Post> findAllPostsByUserId(Long userId);
 }
