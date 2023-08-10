4c7f88e2a914ab7362bf5e54790ed7ca4d07fe13
DianeShin <jadore845@snu.ac.kr>
2023-08-10 23:27:00 +0200

    implement funder comment

--- jump-booster-frontend/src/Post/Post.js
+++ jump-booster-frontend/src/Post/Post.js
@@ -7,25 +7,64 @@ import {ContextProvider} from "../general/ContextElem";
 const Post = (props) => {
     const {userId} = useContext(ContextProvider);
     const [post, setPost] = useState('');
+    const [fundersComments, setFundersComments] = useState([]);
     const [tabMode, setTabMode] = useState('Description');
     const [userName, setUserName] = useState('');
     const [userRole, setUserRole] = useState('');
+    const [commentChange, setCommentChange] = useState(false);
 
     useEffect(() => {
         if (userId !== '') getAccountById(userId).then((userObj) => setUserRole(userObj.role));
+        const fetchData = async () => {
+            const formData = new FormData();
+            formData.append("postId", props.id);
+
+            const postResponse = await fetch("/post/get-post-by-post-id", {
+                method: "POST",
+                body: formData
+            })
+
+            const postData = await postResponse.json();
+            setPost(postData);
+            if (postData.userId) getAccountById(postData.userId).then((userObj) => setUserName(userObj.username));
+
+            const fundersCommentsResponse = await fetch("/comment/get-funders-comments-by-post-id", {
+                method: "POST",
+                body: formData
+            })
+
+            const fundersCommentsData = await fundersCommentsResponse.json();
+
+            const getCommentsPromises = fundersCommentsData.map((comment) =>
+                getAccountById(comment.userId).then((accountObj) => (comment.username = accountObj.username))
+            );
+
+            Promise.all(getCommentsPromises).then(() => {
+                setFundersComments(fundersCommentsData);
+            });
+
+        }
+
+        fetchData();
+    }, [commentChange]);
+
+    function handleCommentSubmit(){
         const formData = new FormData();
-        formData.append("postId", props.id);
-        fetch("/post/get-post-by-post-id", {
+        formData.append("postId", post.postId);
+        formData.append("userId", userId);
+        formData.append("content", document.getElementById('funders-comment-textarea').value);
+        formData.append("role", "funder");
+        fetch("/comment/upload", {
             method: "POST",
             body: formData
         })
-            .then((response) => response.json())
-            .then((postResponse) => {
-                setPost(postResponse);
-                if(postResponse.userId) getAccountById(postResponse.userId).then((userObj) => setUserName(userObj.username));
-            });
-
-    }, []);
+            .then(() => {
+                alert("Comment uploaded!");
+                setCommentChange(!commentChange);
+                document.getElementById('funders-comment-textarea').value = ''
+            })
+            .catch((error) => alert("Something didn't go right."))
+    }
 
     function handleDelete(postId){
         const formData = new FormData();
@@ -47,9 +86,9 @@ const Post = (props) => {
             .catch((error) => alert("Something didn't go right."))
     }
 
-    function handleActive(postId){
+    function handleActive(){
         const formData = new FormData();
-        formData.append("postId", postId);
+        formData.append("postId", post.postId);
         formData.append("userId", userId);
 
         fetch("/post/activate", {
@@ -67,9 +106,9 @@ const Post = (props) => {
             .catch((error) => alert("Something didn't go right."))
     }
 
-    function handleAbort(postId){
+    function handleAbort(){
         const formData = new FormData();
-        formData.append("postId", postId);
+        formData.append("postId", post.postId);
         formData.append("userId", userId);
 
         fetch("/post/abort", {
@@ -97,15 +136,32 @@ const Post = (props) => {
                     <button onClick={() => setTabMode('Funder\'s comments')}>Funder's comments</button>
                     <button onClick={() => setTabMode('Policies')}>Policies</button>
                 </div>
+                {tabMode === 'Funder\'s comments' && (
+                    <div id="funders-comments-div">
+                        <textarea id="funders-comment-textarea" placeholder="Write comment..."></textarea>
+                        <button onClick={handleCommentSubmit}>Submit</button>
+                        {fundersComments && fundersComments.reverse().map(comment => (
+                            <div key={comment.commentId} className="commentDiv">
+                                <div className="commentContent">
+                                    <p>{comment.content}</p>
+                                </div>
+                                <div className="commentInfo right">
+                                    <p>By : {comment.username} </p>
+                                    <p className="timestamp">({comment.timestamp})</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
                 {tabMode === 'Description' && (
-                    <div id = "tab-div">
+                    <div id = "description-div">
                         <p>{<img src={`data:image/jpeg;base64,${post.contentImg}`} alt="contentImg" id="content-img"/>}</p>
                     </div>
 
                 )}
                 {tabMode === 'Policies' && (
-                    <div id = "tab-div">
-                        <p>Funding is investing. We can't guarantee a reward.</p>
+                    <div id = "policies-div">
+                        <p className="content-font">Funding is investing. Funding doesn't guarantee a reward. However, we will try our best to bring the best result!</p>
                     </div>
 
                 )}
@@ -118,13 +174,13 @@ const Post = (props) => {
                         <>
                             {post.status === -1 ? (
                                 <>
-                                    <button onClick={() => handleActive(post.postId)} id="activate-button">Activate</button>
-                                    <button onClick={() => handleDelete(post.postId)} id="delete-button">Delete</button>
+                                    <button onClick={() => handleActive} id="activate-button">Activate</button>
+                                    <button onClick={() => handleDelete} id="delete-button">Delete</button>
                                 </>
                             ) : (
                                 <>
-                                    <button onClick={() => handleAbort(post.postId)} id="abort-button">Abort</button>
-                                    <button onClick={() => handleDelete(post.postId)} id="delete-button">Delete</button>
+                                    <button onClick={() => handleAbort} id="abort-button">Abort</button>
+                                    <button onClick={() => handleDelete} id="delete-button">Delete</button>
                                 </>
                             )}
                         </>
